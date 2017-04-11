b05010cb11aa1f3ffae1b2328901f60852a5adf0
AnujSehgall <anujsehgal.04@gmail.com>
2017-04-11 10:23:53 +0530

    Json parsing

--- app/src/main/java/com/anuj/map/MapsActivity.java
+++ app/src/main/java/com/anuj/map/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.anuj.map;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Geocoder;
@@ -14,12 +15,18 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -27,6 +34,9 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.text.DecimalFormat;
@@ -38,11 +48,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private GoogleMap mMap;
     private TrackGPS gps;
-    double longitude, newLat, newLng;
+    double longitude, newLat, newLng, lat_url,lng_url;
     double latitude;
+    private static String TAG = MapsActivity.class.getSimpleName();
+    private String Respname,Respabout,Resptoll,Respdonate,Respemail,Respadd;
     Button lct;
     public int i;
-
+    public String url="http://api.thingspeak.com/channels/255333/feed/last.json";
+    private ProgressDialog pDialog;
     public LatLng newlatlng,latLng;
 
     @Override
@@ -59,13 +72,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             public void onClick(View view) {
                 Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel: 9742706888"));
                 if (ActivityCompat.checkSelfPermission(MapsActivity.this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
                     return;
                 }
                 startActivity(intent);
@@ -108,30 +114,23 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
     */
 
-        newlatlng = new LatLng(13.119053, 77.578741);
+        newlatlng = new LatLng(lat_url, lng_url);
         lct = (Button) findViewById(R.id.lcote);
         lct.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addOldMark(13.119053, 77.578741);
+                addOldMark(lat_url, lng_url);
             }
         });
 
-
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Please wait...");
+        pDialog.setCancelable(false);
+        makeJsonObjectRequest();
     }
 
 
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -139,11 +138,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         Geocoder gc = new Geocoder(MapsActivity.this);
 
         List<android.location.Address> list = null;
-
-        //LatLng latLng = marker.getPosition();
-
-
-
         try {
             list = gc.getFromLocation(latitude,longitude,1);
             newLat = latitude;
@@ -152,33 +146,16 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             e.printStackTrace();
             return;
         }
-
-        // Toast.makeText(MapsActivity.this,newLat + " , " + newLng, Toast.LENGTH_SHORT).show();
-
         android.location.Address add =   list.get(0);
         String addressLine1 = add.getAddressLine(1);
         String addressLine2 = add.getAddressLine(2);
-        /*marker.setTitle(add.getAddressLine(1));
-        marker.setSnippet(add.getAddressLine(2));
-        marker.showInfoWindow();
-        */// Add a marker in Sydney and move the camera
         latLng= new LatLng(latitude, longitude);
         mMap.addMarker(new MarkerOptions().position(latLng).title(addressLine1).snippet(addressLine2)).setVisible(true);
-
-        //new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.drawable.mark));
-
-
-        // Move the camera instantly to location with a zoom of 15.
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 22));
 
         // Zoom in, animating the camera.
         mMap.animateCamera(CameraUpdateFactory.zoomTo(16), 2000, null);
-
-        //mMap.addMarker(new MarkerOptions().position(latLng).title("Current Location"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-
-
-    }
+ }
 
     private void addOldMark(double lat, double lng) {
         LatLng ll = new LatLng(lat, lng);
@@ -198,8 +175,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             e.printStackTrace();
             return;
         }
-
-        // Toast.makeText(MapsActivity.this,newLat + " , " + newLng, Toast.LENGTH_SHORT).show();
 
         android.location.Address add =   list.get(0);
         String addressLine1 = add.getAddressLine(0);
@@ -278,5 +253,55 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void makeJsonObjectRequest() {
+
+        showpDialog();
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, response.toString());
+
+                try {
+
+                    lat_url = response.getDouble("field1");
+                    lng_url = response.getDouble("field2");
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),
+                            "Error: " + e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                }
+                hidepDialog();
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+                // hide the progress dialog
+                hidepDialog();
+            }
+        });
+
+        // Adding request to request queue
+        AppCont.getInstance().addToRequestQueue(jsonObjReq);
+    }
+
+    private void showpDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hidepDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
     }
 }
